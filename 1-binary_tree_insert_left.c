d734b37532e4373a17eba08789f15312653e4563
abu al ameen <abulyaqs@gmail.com>
2023-08-31 14:28:21 +0100

    all task

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,13 +17,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	new_node->n = value;
-	new_node->parent = parent;
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
