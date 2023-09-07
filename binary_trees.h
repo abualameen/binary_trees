ef44d937ddbd49461aef1bb89805f5805b053428
abu al ameen <abulyaqs@gmail.com>
2023-09-07 22:22:31 +0100

    all task

--- binary_trees.h
+++ binary_trees.h
@@ -3,8 +3,8 @@
 #include <stddef.h>
 #include <stdbool.h>
 /**
- * struct Node - quee
- * @value: the value or data
+ * struct que_node - the que declearation
+ * @n: the value or data
  * @next: pointer to the next node
  */
 
@@ -23,10 +23,10 @@ typedef struct que_node
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -51,7 +51,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 
