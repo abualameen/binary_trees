719c7ec9848748286cc18d6b852a19fef592d0d7
abu al ameen <abulyaqs@gmail.com>
2023-09-11 19:46:27 +0100

    all task

---
+++ 111-bst_insert.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+
+/**
+ * bst_insert - function entry
+ * @tree: double pointer to the tree root
+ * @value: value to be node to be inserted
+ * Return: 0
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *new_node;
+
+	if (*tree == NULL)
+	{
+		new_node = (bst_t *)binary_tree_node((binary_tree_t *)*tree, value);
+		return (new_node);
+	}
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			new_node = (bst_t *)binary_tree_node((binary_tree_t *)*tree, value);
+			(*tree)->left = new_node;
+			return (new_node);
+		}
+		else
+		{
+			return (bst_insert(&((*tree)->left), value));
+		}
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			new_node = (bst_t *)binary_tree_node((binary_tree_t *)*tree, value);
+			(*tree)->right = new_node;
+			return (new_node);
+		}
+		else
+		{
+			(bst_insert(&((*tree)->right), value));
+		}
+	}
+	return (NULL);
+}
