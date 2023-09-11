ff1efa1f2b8fc0955173aa4965827cd941e56506
abu al ameen <abulyaqs@gmail.com>
2023-09-11 10:19:17 +0100

    all task

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
-
+int check_if_bst(binary_tree_t *node, double last_visted_node_value);
 /**
  * binary_tree_is_bst - the entry point
  * @tree: the pointer to the root
  * Return: 0
  */
-int check_if_bst(binary_tree_t *node, double last_visted_node_value);
-
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	double last_visted_node_value = -DBL_MAX;
@@ -17,9 +15,14 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	return (check_if_bst((binary_tree_t *)tree, last_visted_node_value));	
+	return (check_if_bst((binary_tree_t *)tree, last_visted_node_value));
 }
-
+/**
+ * check_if_bst - function entry
+ * @node: pointer to the root
+ * @last_visted_node_value: data
+ * Return: 0
+ */
 int check_if_bst(binary_tree_t *node, double last_visted_node_value)
 {
 	if (node == NULL)
@@ -28,13 +31,12 @@ int check_if_bst(binary_tree_t *node, double last_visted_node_value)
 	}
 	if (!check_if_bst(node->left, last_visted_node_value))
 	{
-		return (0);	
+		return (0);
 	}
 	if (last_visted_node_value != -DBL_MAX && node->n <= last_visted_node_value)
 	{
 		return (0);
 	}
 	last_visted_node_value = node->n;
-	
 	return (check_if_bst(node->right, last_visted_node_value));
 }
